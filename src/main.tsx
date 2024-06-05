72edba844b9731d56cb3d352bb9e6b32d1b20323
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-06-05 21:03:40 +0900

    chore : msw 추가 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { worker } from './mocks/browser'
 
+// if (process.env.NODE_ENV === 'development') {
+//     worker.start()
+// }
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser')
+  return worker.start()
+}
+
+const queryClient = new QueryClient()
+
+
+enableMocking().then(()=>{
+  
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+  )
+})
+
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,116 @@
 // src/mocks/handlers.ts
 import { http, HttpResponse } from 'msw'
- 
+
 export const handlers = [
-  http.get('/api/users', ()=>{
-    return HttpResponse.json([
-      {
-        id : 1,
-        name : "Ham"
+  http.get('https://example.com/products', ()=>{
+    return HttpResponse.json({
+      "response": [
+        {
+          "id": 1,
+          "price": 10000,
+          "name": "치킨",
+          "imageUrl": "http://example.com/chicken.jpg"
+        },
+        {
+          "id": 2,
+          "price": 20000,
+          "name": "피자",
+          "imageUrl": "http://example.com/pizza.jpg"
+        },
+        {
+          "id": 3,
+          "price": 20000,
+          "name": "족발",
+          "imageUrl": "http://example.com/pizza.jpg"
+        },
+        {
+          "id": 4,
+          "price": 20000,
+          "name": "보쌈",
+          "imageUrl": "http://example.com/pizza.jpg"
+        }
+      ]
+    })
+  }),
+  http.get('https://example.com/products/id', ()=>{
+    return HttpResponse.json({
+      "response": {
+        "id": 1,
+        "price": 10000,
+        "name": "치킨",
+        "imageUrl": "http://example.com/chicken.jpg"
       }
-    ])
+    })
+  }),
+  http.get('https://example.com/orderList', ()=>{
+    return HttpResponse.json({
+      "response": [
+        {
+          "id": 1,
+          "orderDetails": [
+            {
+              "id": 1,
+              "price": 10000,
+              "name": "치킨",
+              "imageUrl": "http://example.com/chicken.jpg",
+              "quantity": 5
+            },
+            {
+              "id": 2,
+              "price": 20000,
+              "name": "피자",
+              "imageUrl": "http://example.com/pizza.jpg",
+              "quantity": 3
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "orderDetails": [
+            {
+              "id": 1,
+              "price": 10000,
+              "name": "치킨",
+              "imageUrl": "http://example.com/chicken.jpg",
+              "quantity": 5
+            },
+            {
+              "id": 2,
+              "price": 20000,
+              "name": "피자",
+              "imageUrl": "http://example.com/pizza.jpg",
+              "quantity": 3
+            }
+          ]
+        }
+      ]
+    })
+  }),
+  http.get('https://example.com/order', ()=>{
+    return HttpResponse.json({
+      "response": {
+        "id": 1,
+        "orderDetails": [
+          {
+            "id": 1,
+            "price": 10000,
+            "name": "치킨",
+            "imageUrl": "http://example.com/chicken.jpg",
+            "quantity": 5
+          },
+          {
+            "id": 2,
+            "price": 20000,
+            "name": "피자",
+            "imageUrl": "http://example.com/pizza.jpg",
+            "quantity": 3
+          }
+        ]
+      }
+    })
+  }),
+  http.post('/carts',async ({request}) => {
+    const info = await request.formData()
+    console.log('Logging', info.get('product'))    
   })
 ]
